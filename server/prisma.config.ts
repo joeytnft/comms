8f31e95df25ea7ffec66fd781e895ca7b19191fa
Joe <joeyt5687@gmail.com>
2026-04-30 09:52:21 -0400

    dat

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -4,15 +4,7 @@ import { defineConfig } from 'prisma/config'
 export default defineConfig({
   earlyAccess: true,
   schema: path.join('prisma', 'schema.prisma'),
-  migrate: {
-    async adapter(env) {
-      const { Pool } = await import('pg')
-      const { PrismaPg } = await import('@prisma/adapter-pg')
-      const ssl = env['DATABASE_URL']?.includes('localhost') || env['DATABASE_URL']?.includes('127.0.0.1')
-        ? false
-        : { rejectUnauthorized: false }
-      const pool = new Pool({ connectionString: env['DATABASE_URL'], ssl })
-      return new PrismaPg(pool)
-    },
+  datasource: {
+    url: process.env['DATABASE_URL'],
   },
 })
